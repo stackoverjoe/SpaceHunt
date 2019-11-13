88cbf7743481c6c75287251c88a010395be77297
stackoverjoe <jml27@pdx.edu>
2019-11-13 11:54:48 -0800

    Ship can navigate the map, background music added

--- map.js
+++ map.js
@@ -25,7 +25,12 @@ function generateMap() {
   }
 }
 */
+
+let coords = [0, 0];
+function updateLocation() {}
 function renderMap(X, Y) {
+  coords[0] = X;
+  coords[1] = Y;
   var outer = document.querySelector("#mainMap");
 
   let mapContainer = document.createElement("table");
@@ -39,12 +44,55 @@ function renderMap(X, Y) {
     for (var col = 0; col < Y; ++col) {
       var mapCell = document.createElement("td"),
         mapObj = document.createElement("div");
-      mapCell.className = "map-cell";
-      mapCell.setAttribute("id", col + "," + row);
+      mapCell.className = "gameCell";
+      mapCell.setAttribute("id", col + "-" + row);
       mapObj.className = "gameCell";
+      mapObj.setAttribute("id", row + "--" + col);
       mapCell.appendChild(mapObj);
       mapRow.appendChild(mapCell);
     }
     mapContainer.appendChild(mapRow);
   }
+
+  document.getElementById(
+    "0-0"
+  ).innerHTML = `<div style='text-align: center; color: white'><img style='height: 100%' src='/assets/Titan.png'/></div>`;
 }
+var player = {
+  xcoord: 0,
+  ycoord: 0
+};
+
+document.onkeydown = function(e) {
+  var oldx = player.xcoord;
+  var oldy = player.ycoord;
+  if (e.keyCode === 37) {
+    //left
+    if (player.xcoord > 0) {
+      --player.xcoord;
+    }
+  }
+  if (e.keyCode === 39) {
+    //down
+    if (player.xcoord < coords[0] - 1) ++player.xcoord;
+  }
+  if (e.keyCode === 38) {
+    //up
+    if (player.ycoord > 0) {
+      --player.ycoord;
+    }
+  }
+  if (e.keyCode === 40) {
+    //down
+    if (player.ycoord < coords[1] - 1) {
+      ++player.ycoord;
+    }
+  }
+
+  document.getElementById(
+    `${oldx}-${oldy}`
+  ).innerHTML = `<div class='mapCell'></div>`;
+  document.getElementById(
+    `${player.xcoord}-${player.ycoord}`
+  ).innerHTML = `<div style='text-align: center; color: white'><img style='height: 100%' src='/assets/Titan.png'/></div>`;
+};
