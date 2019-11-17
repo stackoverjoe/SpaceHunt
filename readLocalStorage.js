b4c95c050b15f739809274771ec9f46510f8b28f
jCallon <jascallon99@gmail.com>
2019-11-17 15:39:57 -0800

    Added debug alert and improved error checking

--- readLocalStorage.js
+++ readLocalStorage.js
@@ -16,10 +16,10 @@ class localStoragePackage{
     this.baseSupplies = 200;
     this.baseCredits = 1000;
     this.canDie = 1;
-    //either "random" or an array of coords, access like coordArray[i].xcoord/ycoord
+    //either "random" or an array of coords, access like variableName[i].xcoord/ycoord
     this.wormholes = "random";
     this.spaceStations = "random";
-    //planets, either "random" or a coord, access like coordArray.xcoord/ycoord
+    //planets, either "random" or a coord, access like variableName.xcoord/ycoord
     this.xort = "random";
     this.blarg = "random";
     this.irk = "random";
@@ -28,11 +28,66 @@ class localStoragePackage{
   }
 }
 
-//returns the results packaged in a class of the above
+//==================sh1 people, you don't need to read past this line :)==================
+
+function debugAlert(pkg){
+  var alertMessage = 
+    "DEBUG ALERT" +
+    "\nTo turn me off, comment me out at the bottom of the file." +
+    "\n\nlocalStorage" +
+    "\nmaxX\t\t"        + localStorage.maxX +
+    "\nmaxY\t\t"        + localStorage.maxY +
+    "\nstartX\t\t"      + localStorage.startX +
+    "\nstartY\t\t"      + localStorage.startY +
+    "\nbaseEnergy\t"    + localStorage.baseEnergy +
+    "\nbaseSupplies\t"  + localStorage.baseSupplies +
+    "\nbaseCredits\t"   + localStorage.baseCredits +
+    "\ncanDie\t\t"      + localStorage.canDie +
+    "\nwormholes\t"     + localStorage.wormholes +
+    "\nspaceStations\t" + localStorage.spaceStations +
+    "\nxort\t\t\t"      + localStorage.xort +
+    "\nblarg\t\t"       + localStorage.blarg +
+    "\nirk\t\t\t"       + localStorage.irk +
+    "\naether\t\t"      + localStorage.aether +
+    "\nfoo\t\t\t"       + localStorage.foo +
+
+    "\n\nlocalStoragePackage" +
+    "\nmaxX\t\t"       + pkg.maxX +
+    "\nmaxY\t\t"       + pkg.maxY +
+    "\nstartX\t\t"     + pkg.startX +
+    "\nstartY\t\t"     + pkg.startY +
+    "\nbaseEnergy\t"   + pkg.baseEnergy +
+    "\nbaseSupplies\t" + pkg.baseSupplies +
+    "\nbaseCredits\t"  + pkg.baseCredits
+    "\ncanDie\t\t"     + localStorage.canDie;
+
+  alertMessage += "\nwormholes\t";
+  if(pkg.wormholes === "random") alertMessage += "random";
+  else
+    for(i in pkg.wormholes)
+      alertMessage += " " + i.xcoord + "," + i.ycoord;
+
+  alertMessage += "\nspaceStations\t";
+  if(pkg.spaceStations === "random") alertMessage += "random";
+  else
+    for(i in pkg.spaceStations)
+      alertMessage += " " + i.xcoord + "," + i.ycoord;
+
+  alertMessage +=
+    "\nxort\t\t\t" + pkg.xort.xcoord   + "," + pkg.xort.ycoord +
+    "\nblarg\t\t"  + pkg.blarg.xcoord  + "," + pkg.blarg.ycoord +
+    "\nirk\t\t\t"  + pkg.irk.xcoord    + "," + pkg.irk.ycoord +
+    "\naether\t\t" + pkg.aether.xcoord + "," + pkg.aether.ycoord +
+    "\nfoo\t\t\t"  + pkg.foo.xcoord    + "," + pkg.foo.ycoord;
+
+  alert(alertMessage);
+}
+
+//returns the results packaged in a class of the above, changing any non-default values
 function readLocalStorage(){
   var pkg = new localStoragePackage();
-  //no pass by reference mean a lot of code dumplication :/
-  var dummy;
+  //no pass by reference mean a lot of code duplication :/
+  var dummy, x, y;
 
   //maxX
   dummy = Number(localStorage.getItem("maxX"));
@@ -77,8 +132,18 @@ function readLocalStorage(){
       dummy[i] = dummy[i].split(",");
 
     pkg.wormholes = new Array(dummyLength);
-    for(var i = 0; i < dummyLength; ++i)
-      pkg.wormholes[i] = new coord(Number(dummy[i][0]), Number(dummy[i][1]));
+
+    for(var i = 0; i < dummyLength; ++i){
+      x = Number(dummy[i][0]);
+      y = Number(dummy[i][1]);
+      if(x != "NaN" && x >= 0 && x < pkg.maxX &&
+         y != "NaN" && y >= 0 && y < pkg.maxY)
+        pkg.wormholes[i] = new coord(x, y);
+      else{
+        pkg.wormholes = "random";
+        break;
+      }
+    }
   }
 
   //spaceStations
@@ -90,68 +155,80 @@ function readLocalStorage(){
       dummy[i] = dummy[i].split(",");
 
     pkg.spaceStations = new Array(dummyLength);
-    for(var i = 0; i < dummyLength; ++i)
-      pkg.spaceStations[i] = new coord(Number(dummy[i][0]), Number(dummy[i][1]));
+
+    for(var i = 0; i < dummyLength; ++i){
+      x = Number(dummy[i][0]);
+      y = Number(dummy[i][1]);
+      if(x != "NaN" && x >= 0 && x < pkg.maxX &&
+         y != "NaN" && y >= 0 && y < pkg.maxY)
+        pkg.spaceStations[i] = new coord(x, y);
+      else{
+        pkg.wormholes = "random";
+        break;
+      }
+    }
   }
 
   //xort
   dummy = localStorage.getItem("xort");
   if(dummy){
     dummy = dummy.split(",");
-    pkg.xort = new coord(Number(dummy[0]), Number(dummy[1]));
+    
+    x = Number(dummy[0]);
+    y = Number(dummy[1]);
+    if(x != "NaN" && x >= 0 && x < pkg.maxX &&
+       y != "NaN" && y >= 0 && y < pkg.maxY)
+      pkg.xort = new coord(x, y);
   }
 
   //blarg
   dummy = localStorage.getItem("blarg");
   if(dummy){
     dummy = dummy.split(",");
-    pkg.blarg = new coord(Number(dummy[0]), Number(dummy[1]));
+
+    x = Number(dummy[0]);
+    y = Number(dummy[1]);
+    if(x != "NaN" && x >= 0 && x < pkg.maxX &&
+       y != "NaN" && y >= 0 && y < pkg.maxY)
+      pkg.blarg = new coord(x, y);
   }
 
   //irk
   dummy = localStorage.getItem("irk");
   if(dummy){
     dummy = dummy.split(",");
-    pkg.irk = new coord(Number(dummy[0]), Number(dummy[1]));
+
+    x = Number(dummy[0]);
+    y = Number(dummy[1]);
+    if(x != "NaN" && x >= 0 && x < pkg.maxX &&
+       y != "NaN" && y >= 0 && y < pkg.maxY)
+      pkg.irk = new coord(x, y);
   }
 
   //aether
   dummy = localStorage.getItem("aether");
   if(dummy){
     dummy = dummy.split(",");
-    pkg.aether = new coord(Number(dummy[0]), Number(dummy[1]));
+
+    x = Number(dummy[0]);
+    y = Number(dummy[1]);
+    if(x != "NaN" && x >= 0 && x < pkg.maxX &&
+       y != "NaN" && y >= 0 && y < pkg.maxY)
+      pkg.aether = new coord(x, y);
   }
 
   //foo
   dummy = localStorage.getItem("foo");
   if(dummy){
     dummy = dummy.split(",");
-    pkg.foo = new coord(Number(dummy[0]), Number(dummy[1]));
+
+    x = Number(dummy[0]);
+    y = Number(dummy[1]);
+    if(x != "NaN" && x >= 0 && x < pkg.maxX &&
+       y != "NaN" && y >= 0 && y < pkg.maxY)
+      pkg.foo = new coord(x, y);
   }
   
+  debugAlert(pkg);
   return pkg;
 }
-/* test code
-  //testing
-  localStorage.setItem("wormholes", "1,2 3,4 5,6 7,8 9,10");
-  //testing
-  
-  alert("Read done.\n" +
-    "maxX = " + pkg.maxX + "\n" +
-    "maxY = " + pkg.maxY + "\n" +
-    "startX = " + pkg.startX + "\n" +
-    "startY = " + pkg.startY + "\n" +
-    "baseEnergy = " + pkg.baseEnergy + "\n" +
-    "baseSupplies = " + pkg.baseSupplies + "\n" +
-    "baseCredits = " + pkg.baseCredits + "\n" +
-    "wormholes[0].xcoord = " + pkg.wormholes[0].xcoord + "\n" +
-    "wormholes[0].ycoord = " + pkg.wormholes[0].ycoord + "\n" +
-    "wormholes[1].xcoord = " + pkg.wormholes[1].xcoord + "\n" +
-    "wormholes[1].ycoord = " + pkg.wormholes[1].ycoord + "\n" +
-    "wormholes[2].xcoord = " + pkg.wormholes[2].xcoord + "\n" +
-    "wormholes[2].ycoord = " + pkg.wormholes[2].ycoord + "\n" +
-    "wormholes[3].xcoord = " + pkg.wormholes[3].xcoord + "\n" +
-    "wormholes[3].ycoord = " + pkg.wormholes[3].ycoord + "\n" +
-    "wormholes[4].xcoord = " + pkg.wormholes[4].xcoord + "\n" +
-    "wormholes[4].ycoord = " + pkg.wormholes[4].ycoord + "\n");
-    */
