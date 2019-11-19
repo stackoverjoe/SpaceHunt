feb73722f1d7785a32aa4fb04cdb4072d25af4fc
mahadd29 <mahadd29@users.noreply.github.com>
2019-11-19 12:24:32 -0800

    added to sensors

--- sensors.js
+++ sensors.js
@@ -1,6 +1,6 @@
 function sensors()
 {
-    document.getElementById("deploy").innerHTML = "Turn sensors off";
+    document.getElementById("deploy").innerHTML = "Turn sensors off"; //will eventually go between this and "deploy sensors"
 
     var aroundMe = new Array(12);
     aroundMe[0] = [player.xcoord+1, player.ycoord]
@@ -14,6 +14,30 @@ function sensors()
     aroundMe[8] = [player.xcoord-1, player.ycoord]
     aroundMe[9] = [player.xcoord-2, player.ycoord]
     aroundMe[10] = [player.xcoord-1, player.ycoord+1]
-    aroundMe[3] = [player.xcoord-1, player.ycoord-1]
+    aroundMe[11] = [player.xcoord-1, player.ycoord-1]
     
+    document.getElementById("hidden").style.display = "block"; //reveal div that will hold information about surroundings
+
+   /* for(var i = 0; i < 12; ++i)
+    {
+        //append hidden div with relevant info about surroundings
+    
+    }*/
+
+    var vals = mapObjs.values()
+    for(tile in vals)
+    {
+        console.log(tile)
+    }
+    /*mapObjs.keys().forEach(function(key) {
+        console.log(key)
+    })*/
+    
+  function logMapElements(value, key, map){
+      console.log(`m[${key}] = ${value.type}`)
+  }
+mapObjs.forEach(logMapElements);
+    
+   
+//modify energy consumption (*2 while sensors are on)
 }
