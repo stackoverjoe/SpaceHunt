035f44e16744a83dfa8d1527e9fe12157097fd05
mahadd29 <mahadd29@users.noreply.github.com>
2019-12-03 09:31:11 -0800

    sensors changed to alert.  gazeteer started

--- sensors.js
+++ sensors.js
@@ -1,10 +1,9 @@
 
 function sensors()
 {
-    var x = document.getElementById("sensorlog")
-    x = "";
-    reveal();
-
+    //var x = document.getElementById("sensorlog")
+    //x = "";
+    var todisplay = "Objects Nearby:\n";
     var aroundMe = new Array(12);
     aroundMe[0] = [player.xcoord+1, player.ycoord]
     aroundMe[1] = [player.xcoord+2, player.ycoord]
@@ -18,34 +17,91 @@ function sensors()
     aroundMe[9] = [player.xcoord-2, player.ycoord]
     aroundMe[10] = [player.xcoord-1, player.ycoord+1]
     aroundMe[11] = [player.xcoord-1, player.ycoord-1]
-
-    //for each element in aroundme, check coordinates in that element in the map
-    //if there is something at that tile, add name of object and coordinates to sensorlog
   
-   document.getElementById("sensorlog").value = "";
-
-   for(i = 0; i < aroundMe.length; ++i)
-   {
-    // if(`${aroundMe[i][0]}-${aroundMe[i][1]}`.type != null)
-    if(mapObjs.has(`${aroundMe[i][0]}-${aroundMe[i][1]}`))
+    //clear before adding new tile information
+    document.getElementById("sensorlog").value = "";
+  
+      //for each element in aroundme, check coordinates in that element in the map
+      //if there is something at that tile, add name of object and coordinates to sensorlog
+     for(i = 0; i < aroundMe.length; ++i)
      {
-      document.getElementById("sensorlog").value += mapObjs.get(`${aroundMe[i][0]}-${aroundMe[i][1]}`).type + " ";
-      document.getElementById("sensorlog").value += aroundMe[i];
-      document.getElementById("sensorlog").value += "\n";
+      if(mapObjs.has(`${aroundMe[i][0]}-${aroundMe[i][1]}`))
+       {
+         /*
+        document.getElementById("sensorlog").value += mapObjs.get(`${aroundMe[i][0]}-${aroundMe[i][1]}`).type + " ";
+        document.getElementById("sensorlog").value += aroundMe[i];
+        document.getElementById("sensorlog").value += "\n";
+        */
+       todisplay += mapObjs.get(`${aroundMe[i][0]}-${aroundMe[i][1]}`).type + " ";
+       todisplay += aroundMe[i];
+       todisplay += "\n";
+  
+       }
      }
-   }
+     if(todisplay == "Objects Nearby:\n"){
+       todisplay = "Sensors could not detect any celestial objects";
+     }
+    alert(todisplay);
+    
+    document.getElementById("energy") -=1;
+}
+
+function godmode(){
+  
+  reveal();
 
 }
 
 function reveal() {
-    let x = document.getElementById("sensorlog");
+    var x = document.getElementById("sensorlog");
     if (x.style.display === "none") {
       x.style.display = "block";
-      document.getElementById("deploy").innerHTML = "Turn sensors off";
+   //   document.getElementById("deploy").innerHTML = "Turn sensors off";
+     // document.getElementById("deployed") = 1;
     } else {
       x.value = " ";
       x.style.display = "none";
-      document.getElementById("deploy").innerHTML = "Deploy Sensors";
-      
+     // document.getElementById("deploy").innerHTML = "Deploy Sensors";
+     // document.getElementById("deployed") = 0;
     }
 }
+/*
+function fillLog(){
+
+  var aroundMe = new Array(12);
+  aroundMe[0] = [player.xcoord+1, player.ycoord]
+  aroundMe[1] = [player.xcoord+2, player.ycoord]
+  aroundMe[2] = [player.xcoord+1, player.ycoord+1]
+  aroundMe[3] = [player.xcoord+1, player.ycoord-1]
+  aroundMe[4] = [player.xcoord, player.ycoord+1]
+  aroundMe[5] = [player.xcoord, player.ycoord+2]
+  aroundMe[6] = [player.xcoord, player.ycoord-1]
+  aroundMe[7] = [player.xcoord, player.ycoord-2]
+  aroundMe[8] = [player.xcoord-1, player.ycoord]
+  aroundMe[9] = [player.xcoord-2, player.ycoord]
+  aroundMe[10] = [player.xcoord-1, player.ycoord+1]
+  aroundMe[11] = [player.xcoord-1, player.ycoord-1]
+
+  //clear before adding new tile information
+  document.getElementById("sensorlog").value = "";
+
+    //for each element in aroundme, check coordinates in that element in the map
+    //if there is something at that tile, add name of object and coordinates to sensorlog
+   for(i = 0; i < aroundMe.length; ++i)
+   {
+    if(mapObjs.has(`${aroundMe[i][0]}-${aroundMe[i][1]}`))
+     {
+       /*
+      document.getElementById("sensorlog").value += mapObjs.get(`${aroundMe[i][0]}-${aroundMe[i][1]}`).type + " ";
+      document.getElementById("sensorlog").value += aroundMe[i];
+      document.getElementById("sensorlog").value += "\n";
+      
+     todisplay += mapObjs.get(`${aroundMe[i][0]}-${aroundMe[i][1]}`).type + " ";
+     todisplay += aroundMe[i];
+     todisplay += "\n";
+
+     }
+   }
+
+}
+*/
